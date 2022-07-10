05ed5ddad28e1c9d89ededc4ee3e773f13561b6c
shekar-maker <ramachandran.shekar@gmail.com>
2022-07-10 13:16:18 +0530

    added 3 new models

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,17 @@
 
 
 print("adding all ML related code")
+
+
+def model1():
+    print("adding code ML model 1")
+  
+  
+  
+def model2():
+    print("adding code ML model 2")
+
+
+
+def model3():
+    print("adding code ML model 3")
